828bcee6c8f8d775fa34c7fb1204dc66ddfd153e
Tomás R. <tomaslopezraigal@gmail.com>
2025-07-07 22:49:33 +0200

    Revert "feat: setup tests"
    This reverts commit 9e639977e155d8831c94bdda34ea04b5aa90084c.

--- frontend/src/setupTests.ts
+++
@@ -1,6 +0,0 @@
-import { beforeAll, afterEach, afterAll } from 'vitest'
-import { server } from './mocks/server'
-
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
--- frontend/tests/e2e/home.spec.ts
+++
@@ -1,7 +0,0 @@
-
-import { test, expect } from 'playwright/test'
-
-test('home page loads', async ({ page }) => {
-    await page.goto('http://localhost:5173')
-    await expect(page.getByText('LinkNest')).toBeVisible()
-})
